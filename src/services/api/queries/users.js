a75c7c88f26576412588a7d1857a15cd711f2cfb
Ayo Joseph <josephayo110@gmail.com>
2023-11-14 00:08:37 +0100

    🔧 feat: add user api queries

---
+++ src/services/api/queries/users.js
@@ -0,0 +1,73 @@
+import URI_MAP from '../uris';
+import { axios, getAuthToken } from '../../../config/axios';
+
+export const signup = async (data) => {
+  const res = await axios.post(`${URI_MAP.users.signup()}`, JSON.stringify(data));
+  return res;
+};
+
+export const verifyEmail = async (data) => {
+  const res = await axios.patch(`${URI_MAP.users.verifyEmail()}`, JSON.stringify(data));
+  return res;
+};
+
+export const resendVerificationEmail = async (data) => {
+  const res = await axios.post(`${URI_MAP.users.resendVerificationEmail()}`, JSON.stringify(data));
+  return res;
+};
+
+export const login = async (data) => {
+  const res = await axios.post(`${URI_MAP.users.login()}`, JSON.stringify(data));
+  return res;
+};
+
+export const forgotPassword = async (data) => {
+  const res = await axios.patch(`${URI_MAP.users.forgotPassword()}`, JSON.stringify(data));
+  return res;
+};
+
+export const resetPassword = async (data) => {
+  const res = await axios.patch(`${URI_MAP.users.resetPassword()}`, JSON.stringify(data));
+  return res;
+};
+
+export const changePassword = async (data) => {
+  const res = await axios.patch(`${URI_MAP.users.resetPassword()}`, JSON.stringify(data));
+  return res;
+};
+
+export const viewProfile = async () => {
+  const res = await axios.get(`${URI_MAP.users.profile.view()}`, {
+    headers: {
+      Authorization: getAuthToken()
+    }
+  });
+  return res;
+};
+
+export const editProfile = async (data) => {
+  const res = await axios.patch(`${URI_MAP.users.profile.edit()}`, JSON.stringify(data), {
+    headers: {
+      Authorization: getAuthToken()
+    }
+  });
+  return res;
+};
+
+export const viewRoles = async () => {
+  const res = await axios.get(`${URI_MAP.users.roles.view()}`, {
+    headers: {
+      Authorization: getAuthToken()
+    }
+  });
+  return res;
+};
+
+export const addManager = async (data) => {
+  const res = await axios.get(`${URI_MAP.users.manager.add(data)}`, {
+    headers: {
+      Authorization: getAuthToken()
+    }
+  });
+  return res;
+};
