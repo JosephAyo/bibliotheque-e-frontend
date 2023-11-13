d7ba2102ca2ab50c15f48faa36a6a7b2c52eca41
Ayo Joseph <josephayo110@gmail.com>
2023-11-13 02:07:23 +0100

    🔧 feat: setup react query

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,23 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import 'styles/globals.css';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
+
 const MyApp = ({ Component, pageProps }) => (
-  <ChakraProvider>
-    <Component {...pageProps} />
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
 
 export default MyApp;
