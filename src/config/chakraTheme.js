e47613eeda272d97964429df44da1407280e0df9
Ayo Joseph <josephayo110@gmail.com>
2023-11-14 00:31:26 +0100

    🔧 feat: setup chakra theme

---
+++ src/config/chakraTheme.js
@@ -0,0 +1,188 @@
+export const chakraThemeColors = {
+  primaryFontColor: '#FFFFFF',
+  secondaryFontColor: '#EF4874',
+  plainOldBlue: 'blue',
+  primaryBgColor: '#552EA3',
+  actionPrimary: {
+    default: '#000000',
+    900: '#000000',
+    800: '#000000',
+    700: '#000000',
+    600: '#000000',
+    500: '#000000',
+    400: '#000000',
+    300: '#000000',
+    200: '#000000',
+    100: '#000000',
+    50: '#000000'
+  },
+  actionSecondary: {
+    default: '#EF4874',
+    900: '#EF4874',
+    800: '#EF4874',
+    700: '#EF4874',
+    600: '#EF4874',
+    500: '#EF4874',
+    400: '#EF4874',
+    300: '#EF4874',
+    200: '#EF4874',
+    100: '#EF4874',
+    50: '#EF4874'
+  },
+  primary: {
+    default: '#0066F5',
+    900: '#0066F5',
+    800: '#0066F5',
+    700: '#0066F5CC',
+    600: '#0066F5CC',
+    500: '#0066F599',
+    400: '#0066F599',
+    300: '#0066F566',
+    200: '#0066F566',
+    100: '#0066F533',
+    50: '#0066F533'
+  },
+  lightGray: {
+    default: '#EFEFEF',
+    900: '#EFEFEF',
+    800: '#EFEFEF',
+    700: '#EFEFEFCC',
+    600: '#EFEFEFCC',
+    500: '#EFEFEF99',
+    400: '#EFEFEF99',
+    300: '#EFEFEF66',
+    200: '#EFEFEF66',
+    100: '#EFEFEF33',
+    50: '#EFEFEF33'
+  },
+  darkGray: {
+    default: '##111111',
+    900: '##111111',
+    800: '##111111',
+    700: '##111111CC',
+    600: '##111111CC',
+    500: '##11111199',
+    400: '##11111199',
+    300: '##11111166',
+    200: '##11111166',
+    100: '##11111133',
+    50: '##11111133'
+  }
+};
+
+export const chakraThemeTextStyles = {
+  'headline-1': {
+    fontSize: '96px',
+    lineHeight: '116px',
+    letterSpacing: '-0.015em',
+    fontWeight: 400
+  },
+  'headline-1-medium': {
+    fontSize: '96px',
+    lineHeight: '116px',
+    letterSpacing: '-0.015em',
+    fontWeight: 500
+  },
+  'headline-2': {
+    fontSize: '64px',
+    lineHeight: '77.45px',
+    letterSpacing: '-0.005em',
+    fontWeight: 400
+  },
+  'headline-2-medium': {
+    fontSize: '64px',
+    lineHeight: '77.45px',
+    letterSpacing: '-0.005em',
+    fontWeight: 500
+  },
+  'headline-3': {
+    fontSize: '48px',
+    lineHeight: '58px',
+    letterSpacing: '0em',
+    fontWeight: 400
+  },
+  'headline-3-medium': {
+    fontSize: '48px',
+    lineHeight: '58px',
+    letterSpacing: '0em',
+    fontWeight: 500
+  },
+  'headline-4': {
+    fontSize: '36px',
+    lineHeight: '44px',
+    letterSpacing: '0.0025em',
+    fontWeight: 400
+  },
+  'headline-4-medium': {
+    fontSize: '36px',
+    lineHeight: '44px',
+    letterSpacing: '0.0025em',
+    fontWeight: 500
+  },
+  'headline-5': {
+    fontSize: '24px',
+    lineHeight: '29px',
+    letterSpacing: '0em',
+    fontWeight: 400
+  },
+  'headline-5-medium': {
+    fontSize: '24px',
+    lineHeight: '29px',
+    letterSpacing: '0em',
+    fontWeight: 500
+  },
+  body: {
+    fontSize: '16px',
+    lineHeight: '19px',
+    letterSpacing: '0.005em',
+    fontWeight: 400
+  },
+  'body-medium': {
+    fontSize: '16px',
+    lineHeight: '19px',
+    letterSpacing: '0.005em',
+    fontWeight: 500
+  },
+  'body-bold': {
+    fontSize: '16px',
+    lineHeight: '19px',
+    letterSpacing: '0.005em',
+    fontWeight: 600
+  },
+  button: {
+    fontSize: '16px',
+    lineHeight: '19px',
+    letterSpacing: '0.015em',
+    fontWeight: 500
+  },
+  caption: {
+    fontSize: '14px',
+    lineHeight: '17px',
+    letterSpacing: '0.004em',
+    fontWeight: 400
+  },
+  'caption-medium': {
+    fontSize: '14px',
+    lineHeight: '17px',
+    letterSpacing: '0.004em',
+    fontWeight: 500
+  },
+  'subtile-1': {
+    fontSize: '12px',
+    lineHeight: '15px',
+    letterSpacing: '0.004em',
+    fontWeight: 400
+  },
+  'subtile-2': {
+    fontSize: '8px',
+    lineHeight: '10px',
+    letterSpacing: '0.004em',
+    fontWeight: 400
+  },
+  'subtile-2-medium': {
+    fontSize: '8px',
+    lineHeight: '10px',
+    letterSpacing: '0.004em',
+    fontWeight: 500
+  }
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { chakraThemeColors, chakraThemeTextStyles } from 'config/chakraTheme';
 import 'styles/globals.css';
 
 const queryClient = new QueryClient({
@@ -11,9 +12,36 @@ const queryClient = new QueryClient({
   }
 });
 
+ const theme = extendTheme({
+  colors: chakraThemeColors,
+  textStyles: chakraThemeTextStyles,
+  components: {
+    Text: {
+      baseStyle: (_props) => ({
+        color: chakraThemeColors.primaryFontColor
+      }),
+      variants: {
+        // used as <Text variant="secondary">
+        secondary: (_props) => ({
+          color: chakraThemeColors.secondaryFontColor
+        })
+      }
+    }
+  },
+
+  styles: {
+    global: (_props) => ({
+      // Optionally set global CSS styles
+      body: {
+        color: chakraThemeColors.primaryFontColor
+      }
+    })
+  }
+});
+
 const MyApp = ({ Component, pageProps }) => (
   <QueryClientProvider client={queryClient}>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
     <ReactQueryDevtools initialIsOpen={false} />
