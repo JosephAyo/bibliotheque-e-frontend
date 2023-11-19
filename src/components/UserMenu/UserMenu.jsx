50513170a6fd0ca344a21ff910db0a448c915288
Ayo Joseph <josephayo110@gmail.com>
2023-11-19 03:41:19 +0100

    👽️ 💄 feat: integrate user roles to user menu role selector

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -9,50 +9,57 @@ import {
   RadioGroup,
   VStack
 } from '@chakra-ui/react';
+import useAppStore from 'lib/store';
 import Link from 'next/link';
 import { FaUserCircle } from 'react-icons/fa';
+import { getOr } from 'utils/objects';
 
-const userRoles = [
-  { name: 'borrower', description: 'borrower', id: 'a7da631c-45e4-4490-9569-b92cb5c52654' },
-  {
-    name: 'proprietor',
-    description: 'proprietor',
-    id: '10cd2c32-5355-4668-be92-0d34e653f5d3'
-  },
-  {
-    name: 'librarian',
-    description: 'librarian',
-    id: '633d9319-f661-4452-9173-110fcaacf2cb'
-  }
-];
+const UserMenu = () => {
+  const {
+    userSlice: { currentUser, setCurrentUser }
+  } = useAppStore();
 
-const UserMenu = () => (
-  <Menu>
-    <MenuButton rounded="100%" as={IconButton} aria-label="User" icon={<FaUserCircle />} />
-    <MenuList minWidth="160px">
-      <MenuItem as={Link} href="/users/account" textStyle="caption-medium">
-        Profile settings
-      </MenuItem>
-      <MenuOptionGroup title="Account type" type="radio" textStyle="caption-medium" marginX="12px">
-        <RadioGroup defaultValue={userRoles[0].id}>
-          <VStack
-            alignItems="flex-start"
-            textStyle="caption"
-            textTransform="capitalize"
-            marginLeft="12px">
-            {userRoles.map((role) => (
-              <Radio key={role.id} value={role.id} size="sm">
-                {role.name}
-              </Radio>
-            ))}
-          </VStack>
-        </RadioGroup>
-      </MenuOptionGroup>
-      <MenuItem as={Link} href="/logout" textStyle="caption-medium">
-        Logout
-      </MenuItem>
-    </MenuList>
-  </Menu>
-);
+  return (
+    <Menu>
+      <MenuButton rounded="100%" as={IconButton} aria-label="User" icon={<FaUserCircle />} />
+      <MenuList minWidth="160px">
+        <MenuItem as={Link} href="/users/account" textStyle="caption-medium">
+          Profile settings
+        </MenuItem>
+        <MenuOptionGroup
+          title="Account type"
+          type="radio"
+          textStyle="caption-medium"
+          marginX="12px">
+          <RadioGroup
+            value={getOr(
+              currentUser,
+              'current_role_id',
+              getOr(currentUser, 'user_role_associations.0.role_id', null)
+            )}
+            onChange={(value) => setCurrentUser({ current_role_id: value })}>
+            <VStack
+              alignItems="flex-start"
+              textStyle="caption"
+              textTransform="capitalize"
+              marginLeft="12px">
+              {getOr(currentUser, 'user_role_associations', []).map((useRoleAssociation) => {
+                const { role } = useRoleAssociation;
+                return (
+                  <Radio key={role.id} value={role.id} size="sm">
+                    {role.name}
+                  </Radio>
+                );
+              })}
+            </VStack>
+          </RadioGroup>
+        </MenuOptionGroup>
+        <MenuItem as={Link} href="/logout" textStyle="caption-medium">
+          Logout
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
 
 export default UserMenu;
--- src/lib/slices/createUserSlice.js
+++ src/lib/slices/createUserSlice.js
@@ -1,4 +1,4 @@
-import { isEmpty } from 'lodash';
+import { isEmpty, merge } from 'lodash';
 
 const defaultState = {
   first_name: '',
@@ -6,16 +6,20 @@ const defaultState = {
   email: '',
   is_email_verified: false,
   is_verified: false,
-  user_role_associations: []
+  user_role_associations: [],
+  current_role_id: ''
 };
 
 const createUserSlice = (set) => ({
   currentUser: defaultState,
   setCurrentUser: (currentUser) => {
+    console.log('currentUser :>> ', currentUser);
     set((state) => ({
       userSlice: {
         ...state.userSlice,
-        currentUser: isEmpty(currentUser) ? defaultState : currentUser
+        currentUser: isEmpty(currentUser)
+          ? defaultState
+          : merge(state.userSlice.currentUser, currentUser)
       }
     }));
   }
