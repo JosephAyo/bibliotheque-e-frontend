a255dbb613bf62227ee80c9feb19afc8ca2043e0
Ayo Joseph <josephayo110@gmail.com>
2023-11-21 04:24:08 +0100

    💄 feat: add theme toggle to manage page

--- src/config/layout.js
+++ src/config/layout.js
@@ -3,6 +3,7 @@ export const needAbsoluteThemeToggle = [
   '/login',
   '/signup',
   '/users/account',
+  '/users/manage',
   '/verification'
 ];
 export const needsRightBar = ['/directory', '/shared', '/drive'];
